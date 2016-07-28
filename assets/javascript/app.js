d7c6809866eff922ecda023a5d446ab71e25f3ca
Thomas Zhang <Thomas Zhang>
2016-07-28 19:03:28 -0400

    Deleting duplicate in topics array, wrapping creating of buttons into a function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,49 @@
 $(document).ready(function(){
 
-    //Array of the pre-existing topics to search from
+//Array of the pre-existing topics to search from
 
-    var topics = ['memes', 'dank memes', 'jet fuel can\'t melt steel beams', 'pepe', 'dat boi', 'dickbutt', 'dickbutt', 'zodiac killer', 'trump', 'pokemon go', 'ayy lmao', 'shitposting', 'deal with it', 'spongebob', 'caveman spongebob', 'surprised patrick', 'squidward', 'feels', 'party hard', 'drunk', 'banana', 'lolwut', 'doge', 'shibe', 'swiggity swooty', 'dat booty', 'swag', 'haters gonna hate', 'boss', 'queen bey', 'troll', 'reaction images', 'come at me bro', 'fail', 'rickroll', 'nyan cat', 'rustle jimmies'];
+var topics = ['memes', 'dank memes', 'jet fuel can\'t melt steel beams', 'pepe', 'dat boi', 'dickbutt', 'zodiac killer', 'trump', 'pokemon go', 'ayy lmao', 'shitposting', 'deal with it', 'spongebob', 'caveman spongebob', 'surprised patrick', 'squidward', 'feels', 'party hard', 'drunk', 'banana', 'lolwut', 'doge', 'shibe', 'swiggity swooty', 'dat booty', 'swag', 'haters gonna hate', 'boss', 'queen bey', 'troll', 'reaction images', 'come at me bro', 'fail', 'rickroll', 'nyan cat', 'rustle jimmies'];
 
-    //for loop to go through each item of the topics array and creating a button for that item
+    //create a function to generate the buttons
+    
+    function createButtons() {
 
-    for (i = 0; i < topics.length; i++) {
-        var memeButton = $('<button>'); 
-        memeButton.addClass('btn btn-info memeButtonClass'); //adding classes for CSS
-        memeButton.attr('data-memename', topics[i]); //adding a memeName data attribute which will be the search item
+        //Empty the buttons div so we don't create duplicate sets when adding new buttons
+        $('#memeButtonsDiv').empty();
 
-        memeButton.text(topics[i]); 
-        $("#memeButtonsDiv").append(memeButton); //appending to our HTML div
+        //for loop to go through each item of the topics array and creating a button for that item
+
+        for (i = 0; i < topics.length; i++) {
+            var memeButton = $('<button>'); 
+            memeButton.addClass('btn btn-info memeButtonClass'); //adding classes for CSS
+            memeButton.attr('data-memename', topics[i]); //adding a memeName data attribute which will be the search item
+
+            memeButton.text(topics[i]); 
+            $("#memeButtonsDiv").append(memeButton); //appending to our HTML div
+        }
     }
+
+    createButtons(); //call the create Buttons function
 
     //On click function to add new memes as buttons
 
     $('#addMeme').on('click', function() {
-
-        //taking the text from the input and adding it as a variable
-
+        
+        //taking the text from the input and adding it as a variable. Then push it to the topics array
         var newTerm = $("#meme-input").val().trim();
+        topics.push(newTerm);
 
-        //using JQuery to create a new button, add the class, add the data-memeName, add the text, and then append to correct div. same process as above
-        var addMemeButton = $('<button>');
-        addMemeButton.addClass('btn btn-info memeButtonClass');
-        addMemeButton.attr('data-memename', newTerm);
-        addMemeButton.text(newTerm);
-        $("#memeButtonsDiv").append(addMemeButton);
+        //run the createButtons function which will create a new set of buttons including the new one submitted from the form box
+        createButtons();
 
-        // This line allows us to take advantage of the HTML "submit" property. This way we can hit enter on the keyboard and it registers the search (in addition to clicks).
+        // This line allows us to take advantage of the HTML "submit" property. This way we can hit enter on the keyboard and it registers the search (in addition to clicks)
         return false;
     });
 
     //on click function for all buttons that are clicked. Call API and display GIFs and ratings
 
     $('body').on('click', 'button', function() {
-            
+
             $('#memesAppearDiv').empty(); //Empty any GIFs from previous searches
 
             var memeName = $(this).data('memename'); //Taking the data-memename of the button clicked and storing as variable memeName
@@ -100,3 +106,4 @@ $(document).ready(function(){
         }
     });
 });
+
